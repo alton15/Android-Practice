74c45aa6e340e81648768cbf0c96653fdda3d744
alton15 <alton15@naver.com>
2019-11-05 20:27:31 +0900

    viewholder 사용

--- SportApp/app/src/main/java/com/project/sportapp/MainActivity.java
+++ SportApp/app/src/main/java/com/project/sportapp/MainActivity.java
@@ -1,14 +1,47 @@
 package com.project.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sport> mSportsData;
+    private SportAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+
+        mSportsData = new ArrayList<>();
+
+        mAdapter = new SportAdapter(mSportsData,this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        initData();
     }
+
+    private void initData() {
+
+        mSportsData.clear();
+
+        String[] sportsList = getResources().getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources().getStringArray(R.array.sports_info);
+
+        for(int i = 0; i < sportsList.length; i++) {
+        mSportsData.add(new Sport(sportsList[i], sportsInfo[i]));
+        }
+
+        mAdapter.notifyDataSetChanged();
+    }
+
 }
--- SportApp/app/src/main/java/com/project/sportapp/SportAdapter.java
+++ SportApp/app/src/main/java/com/project/sportapp/SportAdapter.java
@@ -1,25 +1,58 @@
 package com.project.sportapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SportAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+
+public class SportAdapter extends RecyclerView.Adapter<SportAdapter.ViewHolder> {
+
+    private ArrayList<Sport> mSportsData;
+    private Context mContext;
+
+    public SportAdapter(ArrayList<Sport> mSportsData, Context mContext) {
+        this.mSportsData = mSportsData;
+        this.mContext = mContext;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public SportAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View itemView = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder( itemView );
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull SportAdapter.ViewHolder holder, int position) {
+        Sport currentSport = mSportsData.get(position);
+        holder.bindTo(currentSport);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mSportsData.size();
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView mTitleView;
+        private TextView mInfoView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.mTitleView = itemView.findViewById(R.id.title);
+            this.mInfoView = itemView.findViewById(R.id.subTitle);
+        }
+
+        void bindTo(Sport currentSport) {
+            mTitleView.setText(currentSport.getTitle());
+            mInfoView.setText(currentSport.getInfo());
+        }
     }
 }
