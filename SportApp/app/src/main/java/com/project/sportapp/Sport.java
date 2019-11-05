4ab343a32ed53160b9d16f649d5d204053b37af6
alton15 <alton15@naver.com>
2019-11-05 19:15:20 +0900

    Sport App

---
+++ SportApp/app/src/main/java/com/project/sportapp/Sport.java
@@ -0,0 +1,16 @@
+package com.project.sportapp;
+
+public class Sport {
+
+    private String title;
+    private String info;
+
+    Sport(String title, String info) {
+        this.title = title;
+        this.info = info;
+    }
+
+    String getTitle() { return title; }
+    String getInfo() { return info; }
+
+}
