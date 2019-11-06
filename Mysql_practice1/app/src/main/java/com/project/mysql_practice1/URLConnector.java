3f57087ca2a195cd329fa2eab22afdc36f3efcb1
alton15 <alton15@naver.com>
2019-11-06 20:12:12 +0900

    practice Mysql

---
+++ Mysql_practice1/app/src/main/java/com/project/mysql_practice1/URLConnector.java
@@ -0,0 +1,62 @@
+package com.project.mysql_practice1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.Buffer;
+
+public class URLConnector extends Thread {
+
+    private String result;
+    private String URL;
+
+    private URLConnector(String url) {
+        this.URL = url;
+    }
+
+    @Override
+    public void run() {
+        final String output = request(URL);
+        result = output;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    private String request(String urlStr) {
+        StringBuilder output = new StringBuilder();
+
+        try{
+            java.net.URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if(conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                int resCode = conn.getResponseCode();
+                if(resCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while (true) {
+                        line = reader.readLine();
+                        if(line == null) {
+                            break;
+                        }
+                        output.append(line + "\n");
+                    }
+
+                    reader.close();
+                    conn.disconnect();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+}
