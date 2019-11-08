38c7d01c8e0e2d22ad6ff459973dc8d72f1e81e6
alton15 <alton15@naver.com>
2019-11-08 20:19:48 +0900

    php, mysql을 이용한 데이터베이스 사용 예제

--- Mysql_practice1/app/src/main/java/com/project/mysql_practice1/MainActivity.java
+++ Mysql_practice1/app/src/main/java/com/project/mysql_practice1/MainActivity.java
@@ -2,13 +2,113 @@ package com.project.mysql_practice1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    String myJSON;
+
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://localhost/PHP_connection.php");
+    }
+
+    protected void showList() {
+        try {
+            JSONObject jsonObject = new JSONObject(myJSON);
+            peoples = jsonObject.getJSONArray(TAG_RESULTS);
+            for(int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String address = c.getString(TAG_ADD);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_ID, id);
+                persons.put(TAG_NAME, name);
+                persons.put(TAG_ADD, address);
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, personList, R.layout.list_item,
+                    new String[]{ TAG_ID, TAG_NAME, TAG_ADD},
+                    new int[]{R.id.id, R.id.name, R.id.address}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... strings) {
+                String uri = strings[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return  sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                myJSON = s;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
     }
 }
