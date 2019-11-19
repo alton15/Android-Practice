815dbb97e0856fae38733e7e4ab80df42e60c5d6
alton15 <alton15@naver.com>
2019-11-19 10:05:24 +0900

    [Add] proximity sensor

--- Sensor_practice2/app/src/main/java/com/project/sensor_practice2/MainActivity.java
+++ Sensor_practice2/app/src/main/java/com/project/sensor_practice2/MainActivity.java
@@ -2,13 +2,73 @@ package com.project.sensor_practice2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    TextView tv_proximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_proximity = findViewById(R.id.tv_proximity);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(proximitySensor == null) {
+            Toast.makeText(this, "No Proximity Sensor Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if(sensorEvent.values[0] == 0) {
+                tv_proximity.setText("Near : " + String.valueOf(sensorEvent.values[0]));
+            } else {
+                tv_proximity.setText("Far : " + String.valueOf(sensorEvent.values[0]));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        switch (i) {
+            case SensorManager.SENSOR_STATUS_UNRELIABLE:
+                Toast.makeText(this, "UNRELIABLE", Toast.LENGTH_SHORT).show();
+                break;
+            case SensorManager.SENSOR_STATUS_ACCURACY_LOW:
+                Toast.makeText(this, "LOW", Toast.LENGTH_SHORT).show();
+                break;
+            case SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM:
+                Toast.makeText(this, "MEDIUM", Toast.LENGTH_SHORT).show();
+                break;
+            case SensorManager.SENSOR_STATUS_ACCURACY_HIGH:
+                Toast.makeText(this, "HIGH", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
