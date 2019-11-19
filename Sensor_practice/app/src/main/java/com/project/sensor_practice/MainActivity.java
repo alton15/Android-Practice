4b9d8e14e0e006473afb2bbdfd232980fe2276b6
alton15 <alton15@naver.com>
2019-11-19 09:16:10 +0900

    [Add] Sensor code

--- Sensor_practice/app/src/main/java/com/project/sensor_practice/MainActivity.java
+++ Sensor_practice/app/src/main/java/com/project/sensor_practice/MainActivity.java
@@ -2,13 +2,55 @@ package com.project.sensor_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor stepCountSensor;
+    TextView tv_stepCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_stepCounter = findViewById(R.id.tv_stepCounter);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(stepCountSensor == null) {
+            Toast.makeText(this, "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+        
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            tv_stepCounter.setText("Step Count : " + String.valueOf(sensorEvent.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
