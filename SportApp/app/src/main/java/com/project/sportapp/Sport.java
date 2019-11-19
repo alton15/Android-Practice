c948f0dee5c0efee19e6e613462271fd1187541a
alton15 <alton15@naver.com>
2019-11-19 19:39:08 +0900

    imageResourse 추가

--- SportApp/app/src/main/java/com/project/sportapp/Sport.java
+++ SportApp/app/src/main/java/com/project/sportapp/Sport.java
@@ -4,14 +4,17 @@ public class Sport {
 
     private String title;
     private String info;
+    private final int imageResourse;
 
-    Sport(String title, String info) {
+    Sport(String title, String info, int imageResourse) {
         this.title = title;
         this.info = info;
+        this.imageResourse = imageResourse;
     }
 
     String getTitle() { return title; }
     String getInfo() { return info; }
+    int getimageResourse() { return imageResourse; }
 
     public void setInfo(String info) {
         this.info = info;
