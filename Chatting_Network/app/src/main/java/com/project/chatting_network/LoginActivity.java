6168267b64277126d3d72af2a9bb49845c3f86ff
alton15 <alton15@naver.com>
2019-12-24 14:41:15 +0900

    Add project

---
+++ Chatting_Network/app/src/main/java/com/project/chatting_network/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.project.chatting_network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.loginActivity_button_signup)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+}
---
+++ Chatting_Network/app/src/main/java/com/project/chatting_network/SignupActivity.java
@@ -0,0 +1,4 @@
+package com.project.chatting_network;
+
+public class SignupActivity {
+}
